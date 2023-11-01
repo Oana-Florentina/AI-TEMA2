3d6af343616c1282881425bce26a21a41d5f751e
Oana-Florentina <floryflorentina55@gmail.com>
2023-11-01 18:11:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ class Sudoku:
     def __init__(self, board, even_list):
         self.board = np.array(board)
         self.domain_list = [[[] for j in range(9)] for i in range(9)]
+        self.filled = 0
+        self.CELL_COUNT = 81
         for i in range(9):
             for j in range(9):
                 if board[i][j] == 0:
@@ -13,20 +15,77 @@ class Sudoku:
                     else:
                         self.domain_list[i][j] = [val for val in range(1, 10)]
                 else:
+                    self.filled += 1
                     self.domain_list[i][j] = []
+    def get_next_position(self, line, col):
+        #verific daca e liber
+        for j in range(col + 1, 9):
+            if self.board[line][j] == 0:
+                return line, j
 
-        
+        for i in range(line + 1, 9):
+            for j in range(9):
+                if self.board[i][j] == 0:
+                    return i, j
+        return 0, 0
 
-sudoku_puzzle = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    def print_solution(self):
+        print(self.board)
+
+    def solve(self, lin, col):
+        for val in self.domain_list[lin][col]:
+            if Validator.is_valid_move(self.board, lin, col, val):
+                self.board[lin][col]=val
+                self.filled += 1
+                if self.filled == self.CELL_COUNT:
+                    self.print_solution()
+                    exit(0)
+                next_line, next_col = self.get_next_position(lin, col)
+
+                self.solve(next_line, next_col)
+
+                self.board[lin][col] = 0
+                self.filled -= 1
+
+
+class Validator:
+    def is_valid_line(board, line, val):
+        return val not in board[line]
+
+    def is_valid_column(board, col, val):
+        return val not in board[0:9 , col]
+
+    def is_valid_square(board, line, col, val):
+        return val not in board[line // 3 * 3: line // 3 * 3 + 3, col // 3 * 3: col // 3 * 3 + 3]
+
+    def is_valid_move(board, line, col, val):
+        return Validator.is_valid_line(board,line,val) and Validator.is_valid_column(board,col,val) and Validator.is_valid_square(board,line,col,val)
+
+
+solvable_sudoku = [
+    [5, 0, 0, 0, 7, 0, 0, 0, 0],
+    [6, 7, 0, 1, 0, 5, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 0],
+    [4, 0, 0, 8, 0, 3, 0, 0, 0],
+    [0, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 0, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 0],
     [0, 0, 0, 0, 8, 0, 0, 7, 9]
 ]
-even_list = [(0, 2), (0, 3)]
-s = Sudoku(sudoku_puzzle, even_list)
+sudoku_board = [
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 0, 2, 1, 9, 5, 3, 0, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [0, 0, 0, 5, 3, 7, 2, 8, 4],
+    [0, 0, 0, 4, 1, 9, 6, 3, 5],
+    [0, 0, 0, 2, 8, 6, 1, 7, 9]
+]
+even_list = []
+s = Sudoku(solvable_sudoku, even_list)
+line, col = s.get_next_position(0, -1)
+
+s.solve(line, col)
